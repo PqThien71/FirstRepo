548083f1aea40ed65e91016ee3107e155fab1c66
PqThien71 <phamquangthien99@gmail.com>
2021-09-19 16:18:11 +0700

    error acos >1 -> ra so ao

--- dongHocNguoc.py
+++ dongHocNguoc.py
@@ -1,11 +1,11 @@
 import numpy as np
-from numpy.lib.type_check import real
+from numpy.lib.type_check import isreal, real
 import sympy as sp
 import math
 
 x1, x2, x3 = sp.symbols("x1 x2 x3", positive=True, real=True)
 nx1 = sp.symbols("nx1")
-u = np.array([x1, x2, x3])
+u = np.array([50, 50, 0])
 # Thong so hinh hoc robot
 a1 = 40
 a2 = 22
@@ -37,4 +37,23 @@ s12 = nx1**2+(cz0-c1)**2
 s22 = (nx1+2*a1)*2+(cz0-c1)*2
 k = sp.sqrt(a2**2+c3**2)
 theta = np.ones((3, 4))*1j
+print((s12+c2**2-k**2)/(2*(s12**0.5)*c2))
+if isreal(nx1):
+    if isreal(nx1+a1):
+        theta[0][0] = theta[0][1] = math.atan2(cy0, cx0)-math.atan2(b1, nx1+a1)
+        theta[0][2] = theta[0][3] = math.atan2(
+            cy0, cx0)+math.atan2(b1, nx1+a1)-math.pi
+    theta[1][0] = -math.acos((s12+c2**2-k**2) /
+                             (2*(s12**0.5)*c2))+math.atan2(nx1, cz0-c1)
+    theta[1][1] = -theta[1][0]
+
+    if isreal(nx1+2*a1):
+        theta[1][2] = -math.acos((s22+c2**2-k**2) /
+                                 (2*(s22**0.5)*c2))-math.atan2(nx1+2*a1, cz0-c1)
+        theta[1][3] = -theta[1][2]
+    theta[2][0] = math.acos((s12-c2**2-k**2)/(2*c2*k))-math.atan2(a2, c3)
+    theta[2][1] = -theta[2][0]
+    theta[2][2] = math.acos((s22-c2**2-k**2)/(2*c2*k))-math.atan2(a2, c3)
+    theta[2][3] = - theta[2][2]
+
 print(theta)
