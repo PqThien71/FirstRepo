10b4bd4b86faf706b637ed2e60563960b79a7721
PqThien71 <phamquangthien99@gmail.com>
2021-09-26 17:17:55 +0700

    theta123 true

--- dongHocNguoc.py
+++ dongHocNguoc.py
@@ -35,9 +35,12 @@ cz0 = C[3][0]
 print(cx0, cy0, cz0)
 nx1 = sp.sqrt(cx0**2+cy0**2-b1**2)-a1
 s12 = nx1**2+(cz0-c1)**2
-s22 = (nx1+2*a1)*2+(cz0-c1)*2
+s22 = (nx1+2*a1)**2+(cz0-c1)**2
 k = sp.sqrt(a2**2+c3**2)
 theta = np.ones((3, 4))*1j
+print(nx1, s12, s22, k)
+print(cmath.acos((s22+c2**2-k**2) /
+                 (2*s22**0.5*c2)))
 
 if isreal(nx1):
     if isreal(nx1+a1):
@@ -47,15 +50,31 @@ if isreal(nx1):
             cy0, cx0)+math.atan2(b1, nx1+a1)-math.pi
     theta[1][0] = -cmath.acos((s12+c2**2-k**2) /
                               (2*(s12**0.5)*c2))+math.atan2(nx1, cz0-c1)
-    theta[1][1] = -theta[1][0]
+    theta[1][1] = cmath.acos((s12+c2**2-k**2) /
+                             (2*(s12**0.5)*c2))+math.atan2(nx1, cz0-c1)
 
     if isreal(nx1+2*a1):
         theta[1][2] = -cmath.acos((s22+c2**2-k**2) /
                                   (2*(s22**0.5)*c2))-math.atan2(nx1+2*a1, cz0-c1)
-        theta[1][3] = -theta[1][2]
-    theta[2][0] = cmath.acos((s12-c2**2-k**2)/(2*c2*k))-math.atan2(a2, c3)
-    theta[2][1] = -theta[2][0]
-    theta[2][2] = cmath.acos((s22-c2**2-k**2)/(2*c2*k))-math.atan2(a2, c3)
-    theta[2][3] = - theta[2][2]
+        theta[1][3] = cmath.acos((s22+c2**2-k**2) /
+                                 (2*s22**0.5*c2))-math.atan2(nx1+2*a1, cz0-c1)
 
-print("theta=", theta)
+    theta[2][0] = cmath.acos((s12-c2**2-k**2)/(2*c2*k))-math.atan2(a2, c3)
+    theta[2][1] = -cmath.acos((s12-c2**2-k**2)/(2*c2*k))-math.atan2(a2, c3)
+    theta[2][2] = cmath.acos((s22-c2**2-k**2)/(2*c2*k))-math.atan2(a2, c3)
+    theta[2][3] = - cmath.acos((s22-c2**2-k**2)/(2*c2*k))-math.atan2(a2, c3)
+
+theta123 = theta
+
+lst = []
+c23_list = []
+for i in range(0, 4):
+    if isreal(theta[0][i]) == True and isreal(theta[1][i]) == True and isreal(theta[2][i]) == True:
+        lst.append(i)
+
+ind = np.array(lst)
+for i in range(0, ind.shape[0]):
+    c23_temp = cmath.cos(theta123[1][ind[i]]+theta123[2][ind[i]])
+    c23_list.append(c23_temp)
+c23 = np.array(c23_list)
+print(c23)
